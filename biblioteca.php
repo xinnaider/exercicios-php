d329c621c751038ebd56e84022930ba1e1f19f5e
xinnaider <fernandoschnneider@gmail.com>
2022-07-22 13:59:08 -0400

    adicionando biblioteca com funções auxiliares

---
+++ biblioteca.php
@@ -0,0 +1,52 @@
+<?php
+
+function clear () {
+    popen('cls', 'w');
+};
+
+function pularl() {
+    echo "\n";
+    echo "";
+    echo "\n";
+}
+
+function arraySort($array, $on, $order=SORT_DESC){
+    $new_array = array();
+    $sortable_array = array();
+
+    if (count($array) > 0) {
+        foreach ($array as $k => $v) {
+            if (is_array($v)) {
+                foreach ($v as $k2 => $v2) {
+                    if ($k2 == $on) {
+                        $sortable_array[$k] = $v2;
+                    }
+                }
+            } else {
+                $sortable_array[$k] = $v;
+            }
+        }
+
+        switch ($order) {
+            case SORT_ASC:
+                asort($sortable_array);
+            break;
+            case SORT_DESC:
+                arsort($sortable_array);
+            break;
+        }
+
+        foreach ($sortable_array as $k => $v) {
+            $new_array[$k] = $array[$k];
+        }
+    }
+
+    return $new_array;
+}
+
+function removerAcentos($string){
+    return preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/",
+    "/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/",
+    "/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/","/(ç)/"),
+    explode(" ","a A e E i I o O u U n N c"),$string);
+}
