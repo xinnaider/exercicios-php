b59a70aaca91b4fdbc7c27c8b3cfd65fc1a0fedb
xinnaider <fernandoschnneider@gmail.com>
2022-08-12 13:35:01 -0400

    att

--- lista4.php
+++ lista4.php
@@ -6,7 +6,7 @@ $connection = pg_connect("dbname=db_papelaria host=localhost port=5432 password=
 
 function adicionarProduto ($connection){
 
-    function headerProdutos (){
+    function headerProdutos(){
         clear();
         print "==================";
         print " INSERIR PRODUTOS ";
@@ -92,44 +92,3 @@ function visualizarProdutos ($connection){
 }
 
 alterProduto ($connection);
-
-function headerProdutos (){
-    clear();
-    print "==================";
-    print " INSERIR PRODUTOS ";
-    print "================== \n \n";
-}
-
-function selectProduto($connection){
-
-    $jorge = pg_query($connection, "select * from produto");
-
-    $line = pg_fetch_all($jorge);
-
-    print_r($line);
-}
-
-headerProdutos();
-$nomepro = readline("Nome: ");
-
-headerProdutos();
-$marcapro = readline("Marca: ");
-
-headerProdutos();
-$precopro = readline("Preço: ");
-
-headerProdutos();
-$qtdpro = readline("Quantidade: ");
-
-$hora = date("h:i:s");
-$data = date("Y/m/d");
-
-pg_query($connection, "INSERT INTO produto (nome_pro, marca_pro, quantidade_pro, preco_pro) VALUES ('$nomepro', '$marcapro', $qtdpro, $precopro)");
-
-$idAuto = pg_query($connection, "SELECT * FROM produto WHERE (nome_pro ='$nomepro')");
-$idAuto = pg_fetch_all($idAuto);
-
-pg_query($connection, "INSERT INTO movimentacoes(tipo_mov, data_mov, id_produto_fk) VALUES ('E','$data $hora', $idAuto[id_pro]);");
-
-selectProduto($connection);
-
